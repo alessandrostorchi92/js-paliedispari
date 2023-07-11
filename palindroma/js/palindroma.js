0509d4759f9da64aab48bd6470d0ded9579855cd
alessandrostorchi92 <storchi.alle@gmail.com>
2023-07-11 10:55:42 +0200

    Esercizio Palindromo Corretto

--- palindroma/js/palindroma.js
+++ palindroma/js/palindroma.js
@@ -3,15 +3,14 @@
 // Procedimento: Chiedi all'utente di scrivere una parola tramite input
 // 1) Aggiungi alla costante .value per poter leggere quello che l’utente ha inserito nel input.
 // 2) Utilizza la funzione addEventListenerper far partire l'evento a seguito del click dell'utente sulla cta
-// 3) Attraverso .split, .reverse, .join pongo le 
-
+// 3) Attraverso .split, .reverse, .join
 
 
 const btnFind = document.querySelector(".btn_find");
 
 btnFind.addEventListener("click", function () {
 
-    const userWordImput = document.querySelector("[name='userword']").value;
+    const userWordImput = document.querySelector("[name='userword_input']").value;
 
     theWordIsPalindrome() // Invoco la funzione per verificare se la funzione è palindroma
 
@@ -25,7 +24,7 @@ btnFind.addEventListener("click", function () {
         let wordJoin = wordReverse.join('');
 
         //  Se la parola è uguale a quella nuova unita nella funzione con .join, allora la parola è palindroma altrimenti non lo è 
-        if (userWord === wordJoin) {
+        if (userWordImput === wordJoin) {
             console.log("La parola è palindroma");
         } else {
             console.log("La parola non è palindroma");

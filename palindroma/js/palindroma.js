52e81af37a4b6666fa671c62e20f482a2863e04b
alessandrostorchi92 <storchi.alle@gmail.com>
2023-07-11 11:57:50 +0200

    Esercizio Finito

--- palindroma/js/palindroma.js
+++ palindroma/js/palindroma.js
@@ -17,12 +17,15 @@ btnFind.addEventListener("click", function () {
 
     function theWordIsPalindrome() {
 
-        // Con .split la parola inserita dall'utente viene convertita in una sorta di array contentente ogni lettera della singola parola  divise da una virgola
+        // Con .split la parola inserita dall'utente viene convertita in una sorta di array contentente ogni lettera della singola parola divise da una virgola. Con una stringa vuota ("") divide la parola in ciascuna lettera che la compone
         let wordArray = userWordImput.split('');
+        console.log(wordArray);
         // Con .reverse l'ordine dell'array creato viene invertito
         let wordReverse = wordArray.reverse();
+        console.log(wordReverse);
         // Con .join viene tolta la virgola e si uniscono tutte le lettere formando una nuova parola
         let wordJoin = wordReverse.join('');
+        console.log(wordJoin);
 
         //  Se la parola è uguale a quella nuova unita nella funzione con .join, allora la parola è palindroma altrimenti non lo è 
         if (userWordImput === wordJoin) {
